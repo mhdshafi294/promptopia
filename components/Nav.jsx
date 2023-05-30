4aae628f674b2f5c3fcad07c30bc6a9949e0e462
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-30 22:54:02 +0300

    working on auth and the backend

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,19 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(()=>{
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   },[])
 
   return (
@@ -33,9 +34,11 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+      {alert(providers)}
+
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/Create-prompt"
             className='black_btn'>
@@ -74,9 +77,9 @@ const Nav = () => {
         )}
       </div>
 
-      {/* Desktop Navigation */}
+      {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
