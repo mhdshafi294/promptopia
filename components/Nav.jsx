5d2e4666979d524b3b3300dbcbdb95062d3edcdf
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-31 22:25:03 +0300

    auth and login with google done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -34,8 +34,6 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
-      {alert(providers)}
-
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
         {session?.user ? (
@@ -51,7 +49,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -82,7 +80,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
