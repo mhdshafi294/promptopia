08bf78d56a21f2c0a87098b530d06ca39f40b201
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-06-01 16:58:01 +0300

    working on db connection error

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,45 +1,41 @@
 "use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
-  const [toggleDropDown, setToggleDropDown] = useState(false);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  useEffect(()=>{
-    const setUpProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    }
-
-    setUpProviders();
-  },[])
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href="/" className="flex gap-2 flex-center">
+      <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src="/assets/images/logo.svg"
-          alt="Promptopia Logo"
+          src='/assets/images/logo.svg'
+          alt='logo'
           width={30}
           height={30}
-          className="object-contain"
+          className='object-contain'
         />
-        <p className="logo_text">Promptopia</p>
+        <p className='logo_text'>Promptopia</p>
       </Link>
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
         {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link href="/Create-prompt"
-            className='black_btn'>
+            <Link href='/create-prompt' className='black_btn'>
               Create Post
             </Link>
 
@@ -47,30 +43,31 @@ const Nav = () => {
               Sign Out
             </button>
 
-            <Link href="/profile">
+            <Link href='/profile'>
               <Image
                 src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
-                alt="profile"
+                alt='profile'
               />
             </Link>
           </div>
-        ): (
+        ) : (
           <>
             {providers &&
-              Object.values(providers).map(provider => (
+              Object.values(providers).map((provider) => (
                 <button
-                  type="button"
+                  type='button'
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
-              ))
-            }
+              ))}
           </>
         )}
       </div>
@@ -78,36 +75,36 @@ const Nav = () => {
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
         {session?.user ? (
-          <div className="flex">
+          <div className='flex'>
             <Image
               src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
-              alt="profile"
-              onClick={() => setToggleDropDown((prev) => !prev)}
+              alt='profile'
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
-            {toggleDropDown && (
+            {toggleDropdown && (
               <div className='dropdown'>
                 <Link
-                  href="/profile"
+                  href='/profile'
                   className='dropdown_link'
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
-                  href="/create-prompt"
+                  href='/create-prompt'
                   className='dropdown_link'
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   Create Prompt
                 </Link>
                 <button
                   type='button'
                   onClick={() => {
-                    setToggleDropDown(false);
+                    setToggleDropdown(false);
                     signOut();
                   }}
                   className='mt-5 w-full black_btn'
@@ -117,25 +114,26 @@ const Nav = () => {
               </div>
             )}
           </div>
-        ):(
+        ) : (
           <>
             {providers &&
-              Object.values(providers).map(provider => (
+              Object.values(providers).map((provider) => (
                 <button
-                  type="button"
+                  type='button'
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
-              ))
-            }
+              ))}
           </>
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
