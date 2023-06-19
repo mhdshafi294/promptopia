1cdc55895a457e040e6e28ea0fef4a8b6547a104
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-06-19 23:29:44 +0300

    starting the create prompt app

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const CreatePrompt = () => {
+  return <div>CreatePrompt</div>;
+};
+
+export default CreatePrompt;
