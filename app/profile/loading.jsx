cfd763f15826e2b0ad8485cd365f1a7bd425110e
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-06-22 00:01:53 +0300

    Prompt update, delete and other things all done!

--- app/profile/loading.jsx
+++ app/profile/loading.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ProfileLoding = () => {
-  return <div>ProfileLoding</div>;
+  return (
+    <div className="flex-center w-full">
+      <Image
+        src="assets/icons/loader.svg"
+        width={50}
+        height={50}
+        alt="loader"
+        className="object-contain"
+      />
+    </div>
+  );
 };
 
 export default ProfileLoding;
